ba541c06305b86c410153ab0621cb977057a1ba7
Brand28 <alejandrobrand28@gmail.com>
2021-05-11 10:14:35 -0500

    clases user, order y extra llenas con atributos y constructores

--- src/model/Order.java
+++ src/model/Order.java
@@ -1,5 +1,67 @@
 package model;
 
-public class Order {
+import java.util.ArrayList;
+import java.util.Date;
 
+public class Order {
+	private String dishName;
+	private Date orderDate;
+	private int orderCuantity;
+	
+	private ArrayList<Extra> extras;
+	
+	private double totalPrice;
+	
+	public Order(String dishName, int orderCuantity, Date orderDate) {
+		this.dishName = dishName;
+		this.orderCuantity = orderCuantity;
+		this.orderDate = orderDate;
+		
+		totalPrice = 0;
+		extras = new ArrayList<Extra>();
+	}
+
+	public String getDishName() {
+		return dishName;
+	}
+
+	public void setDishName(String dishName) {
+		this.dishName = dishName;
+	}
+
+	public Date getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public int getOrderCuantity() {
+		return orderCuantity;
+	}
+
+	public void setOrderCuantity(int orderCuantity) {
+		this.orderCuantity = orderCuantity;
+	}
+
+	public ArrayList<Extra> getExtras() {
+		return extras;
+	}
+
+	public double getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+	
+	public void addExtra() {
+		
+	}
+	
+	public void calculateTotal() {
+		
+	}
 }
--- src/model/User.java
+++ src/model/User.java
@@ -1,5 +1,52 @@
 package model;
 
-public class User {
+import java.util.ArrayList;
 
+public class User {
+	private String name;
+	private String email;
+	private String password;
+	
+	private ArrayList<Order> orderList;
+	
+	public User(String name, String email, String password) {
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		
+		orderList = new ArrayList<Order>();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void newOrder() {
+		
+	}
+
+	public ArrayList<Order> getOrderList() {
+		return orderList;
+	}
+	
 }

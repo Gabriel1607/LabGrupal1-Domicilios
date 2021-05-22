8bc109a7d2da7fddf908d283aa6598d55448d995
Gabriel <gabrielmora2001@hotmail.com>
2021-05-22 08:30:59 -0500

    Clase screen creada

--- src/view/Screen.java
+++ src/view/Screen.java
@@ -1,5 +1,23 @@
 package view;
 
-public abstract class Screen {
+import processing.core.PApplet;
+import processing.core.PImage;
 
+public abstract class Screen {
+	  protected PApplet app;
+	  PImage Login,Register,Home,ExtrasCorral,ExtrasTocineta,ExtrasPollo,ExtrasQueso,Details,Pay,History;
+	  public Screen(PApplet app) {
+		  this.app = app;
+		  Login = app.loadImage ("img/Login.png");
+		  Register = app.loadImage ("img/Register.png");
+		  Home = app.loadImage ("img/Home.png");
+		  ExtrasCorral = app.loadImage ("img/ExtrasCorral.png");
+		  ExtrasQueso = app.loadImage ("img/ExtrasQueso.png");
+		  ExtrasTocineta = app.loadImage ("img/ExtrasTocineta.png");
+		  ExtrasPollo = app.loadImage ("img/ExtrasPollo.png");
+		  Details = app.loadImage ("img/Details.png");
+		  Pay = app.loadImage ("img/Pay.png");
+		  History = app.loadImage ("img/History.png");
+	  }
+	  public abstract void draw (PApplet app);
 }

a8a93b54a90c1cf01c92c7b6192732e8ce8604b8
erika2201 <erikapapamija@gmail.com>
2021-05-22 09:32:23 -0500

    Casos de switch listos

--- src/model/Logic.java
+++ src/model/Logic.java
@@ -1,22 +1,55 @@
 package model;
 
 import processing.core.PApplet;
+import view.AddExtraScreen;
+import view.CompletePayScreen;
+import view.HomeScreen;
 import view.LogInScreen;
+import view.OrderHistoryScreen;
+import view.OrderSummaryScreen;
+import view.RegisterScreen;
 
 public class Logic {
 	private PApplet app ;
 	int screen;
 	LogInScreen login;
+	RegisterScreen register;
+	HomeScreen home;
+	AddExtraScreen extra;
+	CompletePayScreen pay;
+	OrderSummaryScreen orderSummary;
+	OrderHistoryScreen history;
+	
 	public Logic(PApplet app) {
 		this.app=app;
 		screen = 1;
 		login = new LogInScreen(app);
 	}
+	
 	public void changeScreen() {
 		switch(screen) {
 		case 1:
 			login.draw();
 			break;
+		case 2:
+			register.draw();
+			break;
+		case 3:
+			home.draw();
+			break;
+		case 4:
+			extra.draw();
+			
+			//Si le da a realizar pago, llama el método draw de de pay
+			pay.draw();
+			break;
+		case 5:
+			orderSummary.draw();
+			break;
+		case 6:
+			history.draw();
+			break;
+
 		}
 	}
 }
--- src/view/CompletePayScreen.java
+++ src/view/CompletePayScreen.java
@@ -1,5 +1,17 @@
 package view;
 
-public class CompletePayScreen {
+import processing.core.PApplet;
+
+public class CompletePayScreen extends Screen {
+	 
+	public CompletePayScreen(PApplet app) {
+	        super(app);
+	    }
+
+	@Override
+	public void draw() {
+		  app.image(Pay,0,0);
+		
+	}
 
 }
